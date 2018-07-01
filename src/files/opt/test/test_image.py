55165c786f6ad2aa20b6669a3bb4aaa6c610bf1a
Fernando Correia <fernandoacorreia@gmail.com>
2018-07-01 10:29:31 -0700

    Remove test of a specific Django version

--- src/files/opt/test/test_image.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import django
-
-
-class ImageTestCase(unittest.TestCase):
-    def test_django_installed(self):
-        django_version = django.get_version()
-        self.assertEqual(django_version, "2.0.6")
